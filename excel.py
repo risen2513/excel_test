4ba407997602892ff3ad6c21b93ba8d17f71cc64
Risen <risen2513@gmail.com>
2017-03-30 21:33:18 +0800

    从源Excel文件将指定数据写入到目标Excel相同id的指定列中

--- excel.py
+++ excel.py
@@ -1,20 +1,32 @@
+# -*- coding: utf-8 -*-
 import xlrd
-import xlwt
+# import xlwt
+from xlutils.copy import copy
 
-
-def excel_job(src,target):
-
+def excel_job(src, target):
     src_book = xlrd.open_workbook(src)
-    sh = src_book.sheet_by_index(0)
-    value = sh.cell_value(rowx=1, colx=0)
+    sh = src_book.sheet_by_name(u'テスト')
+    # value = sh.cell_value(rowx=1, colx=0)
 
-    target_book = xlwt.Workbook()
-    sheet2 = target_book.get_sheet(0)
-    sheet2.row(1).write(0, value)
-    sheet2.flush_row_data()
-    target_book.save(target)
+    oldWb = xlrd.open_workbook(target, formatting_info=True)
+    shwt = oldWb.sheet_by_name(u'hello')
+    newWb = copy(oldWb)
+    sheet = newWb.get_sheet('hello')
+    src_id = [[] for i in range(2)]
+    for irow in range(sh.nrows):
+        c_row = sh.row(irow)
+        src_id[0].append(str(c_row[0].value))
+        src_id[1].append(c_row[1].value)
 
+    for jrow in range(shwt.nrows):
+        d_row = shwt.row(jrow)
+        for srci_row in range(len(src_id[0])):
+            if str(src_id[0][srci_row]) == str(d_row[0].value):
+                print str(src_id[0][srci_row])
+                sheet.write(jrow, 1, src_id[1][srci_row])
+
+    newWb.save(target)
 
 
 if __name__ == '__main__':
-    excel_job('src.xls', 'target')
+    excel_job('src.xls', 'target.xls')
